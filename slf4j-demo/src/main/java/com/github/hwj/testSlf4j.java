ccff900b7659b4f200c60d06cf139dd5b0f48cb7
Master-He <478243824@qq.com>
2022-05-31 09:14:32 +0800

    slf4j-demo

---
+++ slf4j-demo/src/main/java/com/github/hwj/testSlf4j.java
@@ -0,0 +1,11 @@
+package com.github.hwj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class testSlf4j {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(testSlf4j.class);
+        logger.info("Hello World");
+    }
+}
---
+++ slf4j-demo/src/main/java/com/github/hwj/testSlf4jLombok.java
@@ -0,0 +1,10 @@
+package com.github.hwj;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class testSlf4jLombok {
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}
