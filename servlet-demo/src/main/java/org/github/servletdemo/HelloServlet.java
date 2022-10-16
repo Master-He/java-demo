2c4a9242bd332063f2165744fc22b89506d68da3
Master-He <478243824@qq.com>
2022-10-16 17:39:38 +0800

    add tomcat/servlet demo

---
+++ servlet-demo/src/main/java/org/github/servletdemo/HelloServlet.java
@@ -0,0 +1,36 @@
+package org.github.servletdemo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author hewenji
+ * @Date 2022/10/16 17:20
+ */
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init() throws ServletException
+    {
+        // 执行必需的初始化
+        message = "Hello World";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>" + message + "</h1>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
